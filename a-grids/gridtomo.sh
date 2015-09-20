fa13dda9e6b75efd66865427ef9a7caf6d8f94b0
mprocha <marcelorocha.unb@gmail.com>
2015-09-20 17:23:18 -0300

    Atualização Sun Sep 20 17:23:04 BRT 2015: varia modificacoes no arquivo RSB-BR-stations.gmt

--- a-grids/gridtomo.sh
+++ a-grids/gridtomo.sh
@@ -2,13 +2,13 @@
 
 #minmax 150km.dat
 
-awk '{print $1,$2,$3}' ~/maps/a-grids/150km.dat > 167.txt
+awk '{print $1,$2,$3}' $HOME/maps/a-grids/150km.dat > 167.txt
 
-xyz2grd ~/maps/a-grids/167.txt -G/home/marcelo/maps/a-grids/167.grd -R-63/-30/-31/-3 -I0.333 -V
+xyz2grd $HOME/maps/a-grids/167.txt -G$HOME/maps/a-grids/167.grd -R-63/-30/-31/-3 -I0.333 -V
 
-grdcut ~/maps/a-grids/167.grd -G/home/marcelo/maps/a-grids/167cut.grd -R-57/-36/-25/-8.66667
+grdcut $HOME/maps/a-grids/167.grd -G$HOME/maps/a-grids/167cut.grd -R-57/-36/-25/-8.66667
 
-makecpt -I -T-.55/.55/0.01 > ~/maps/a-grids/167tmp.cpt
+makecpt -I -T-.55/.55/0.01 > $HOME/maps/a-grids/167tmp.cpt
 
-grdimage ~/maps/a-grids/167cut.grd -JM19 -E300 -C/home/marcelo/maps/a-grids/167tmp.cpt > quick.ps
+grdimage $HOME/maps/a-grids/167cut.grd -JM19 -E300 -C$HOME/maps/a-grids/167tmp.cpt > quick.ps
 gv quick.ps&
